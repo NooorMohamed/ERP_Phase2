7d158c74a2431945a2732291894ab39d8746ac0a
NooorMohamed <noormohamed.28.96@gmail.com>
2018-05-04 16:22:37 +0200

    App

--- erp/src/App.js
+++ erp/src/App.js
@@ -5,11 +5,16 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Biography from './Biography.js';
 import Multimedia from './Multimedia.js';
 import Speeches from './Speeches.js'
+import News from './News/News.js';
+
 class App extends Component {
   render() {
     return [
-    <div>
+   /* <div>
      <Speeches/>    
+    </div>*/
+    <div>
+        <News />
     </div>
         /*
       ,<div>
